f6a21b86123116e3317acf7b7ad051331a849079
Murzik-boss <cool.developer360@gmail.com>
2025-01-10 03:08:46 +1000

    works

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-function askName() {
+function ask() {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 }
-export { askName };
+export default ask;

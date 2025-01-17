90e1cfc3cf44e21fa400535a8e4beefd6ee1f771
Murzik-boss <cool.developer360@gmail.com>
2025-01-17 17:24:08 +1000

    node bin/brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 function ask() {
-  const userName = readlineSync.question("May I have your name? ");
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   return userName;
 }
@@ -14,32 +14,30 @@ function getRandomNumber() {
 
 function isEven(number) {
   if (number % 2 === 0) {
-    return "yes";
-  } else {
-    return "no";
+    return 'yes';
   }
+  return 'no';
 }
 
 function goGame() {
   console.log("Answer 'yes' if number even otherwise answer 'no'");
   let isCorrectGame = true;
   for (let i = 0; i < 3; i++) {
-    let questionNumber = getRandomNumber();
-    console.log("Question: " + questionNumber);
-    let userAnswer = readlineSync.question("Your answer: ");
+    const questionNumber = getRandomNumber();
+    console.log(`Question: ${questionNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === isEven(questionNumber)) {
-      console.log("Correct!");
+      console.log('Correct!');
     } else {
       console.log(
-        '"yes" is wrong answer ;(. Correct answer was "no".\n Let\'s try again ' +
-          userName
+        `"yes" is wrong answer ;(. Correct answer was "no".\n Let's try again ${userName}`,
       );
       isCorrectGame = false;
       break;
     }
   }
   if (isCorrectGame) {
-    console.log("Congratulations, " + userName + "!");
+    console.log(`Congratulations, ${userName}!`);
   }
 }
 goGame();
--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 function ask() {
-  const userName = readlineSync.question("May I have your name? ");
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 }
 export default ask;
